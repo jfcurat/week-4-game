f041e46016dbd7a6ebfe553fc7b81b7ca964cba5
James Curatalo <jfcurat@gmail.com>
2017-12-17 13:59:28 -0800

    Starting Vars
    started declaring variables needed to keep track of scores
    added jQuery document ready check

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	var roundScoreGoal = 0; //use Math.random to pick a number between 19-120
+	var roundScoreCounter = 0; //keep track of round score
+
+	var winsCounter = 0; //keep track of wins and losses over multiple rounds
+	var lossesCounter = 0;
+
+	var redCrystalValue = 0; //use Math.random to pick a number between 1-12 for each crystalValue
+	var blueCrystalValue = 0;
+	var yellowCrystalValue = 0;
+	var greenCrystalValue = 0;
+
+
+});
