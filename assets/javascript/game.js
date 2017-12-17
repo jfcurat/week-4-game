f07492a69c1aa59acc5f9635a191e53487fd3a98
James Curatalo <jfcurat@gmail.com>
2017-12-17 15:58:29 -0800

    JS Comments and Bootstrap
    added comments to help organize javascript thought process
    added Bootstrap classes to the main section
    not final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,5 +10,34 @@ $(document).ready(function() {
 	var yellowCrystalValue = 0;
 	var greenCrystalValue = 0;
 
+	/* things needed?
 
+	function to clear roundScoreCounter/$("#roundScoreCounterDisplay"). and start new round
+		assign new the crystalValues and new roundScoreGoal/$("#roundScoreGoalDisplay").
+			maybe just call other functions that deal with crystalValue assignment and roundScoreGoal assignment
+				after clearing stuff
+
+	function to use Math.random() to pick number between 19-120 for roundScoreGoal
+
+	function to use Math.random() to pick numbers between 1-12 for red/blue/yellow/greenCrystalValue
+
+	function to 
+
+	function to keep track of roundScoreCounter and check
+		if (roundScoreCounter === roundScoreGoal) {
+			winsCounter++;
+			$("#winsCounterDisplay").text(winsCounter)
+		}
+		else if (roundScoreCounter > roundScoreGoal) {
+			lossesCounter++;
+			$("#lossesCounterDisplay").text(lossesCounter)
+		}
+
+	function to 
+
+	*/
+
+	function() {
+
+	}
 });
