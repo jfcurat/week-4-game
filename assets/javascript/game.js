384314ae98574c7981b30a6a1857b2dbdaa3b1fd
James Curatalo <jfcurat@gmail.com>
2017-12-17 20:19:53 -0800

    Added Random Number Functions
    added random number functions for crystals and scoreGOal
    fixed some errors in index.html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,13 +16,51 @@ $(document).ready(function() {
 		assign new the crystalValues and new roundScoreGoal/$("#roundScoreGoalDisplay").
 			maybe just call other functions that deal with crystalValue assignment and roundScoreGoal assignment
 				after clearing stuff
+	*/
+	//function roundStarter() {
+	//	roundScoreGoal = 
+	//}
 
-	function to use Math.random() to pick number between 19-120 for roundScoreGoal
+	
+	//function to use Math.random() to pick number between 19-120 for roundScoreGoal
+	function getRandomScoreGoal(min, max) {
+		min = Math.ceil(19);
+		max = Math.floor(120);
+		return Math.floor(Math.random() *  (max - min + 1)) + min; //max inclusive and min inclusive
+	}
+	console.log(getRandomScoreGoal());
 
-	function to use Math.random() to pick numbers between 1-12 for red/blue/yellow/greenCrystalValue
+	//functions to use Math.random() to pick numbers between 1-12 for red/blue/yellow/greenCrystalValue
+	function getRandomRedCrystalValue(min, max) {
+		min = Math.ceil(1);
+		max = Math.floor(12);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+	console.log(getRandomRedCrystalValue());
 
-	function to 
+	function getRandomBlueCrystalValue(min, max) {
+		min = Math.ceil(1);
+		max = Math.floor(12);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+	console.log(getRandomBlueCrystalValue());
+	
+	function getRandomYellowCrystalValue(min, max) {
+		min = Math.ceil(1);
+		max = Math.floor(12);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+	console.log(getRandomYellowCrystalValue());
 
+	function getRandomGreenCrystalValue(min, max) {
+		min = Math.ceil(1);
+		max = Math.floor(12);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+	console.log(getRandomGreenCrystalValue());
+	//function to 
+
+	/*
 	function to keep track of roundScoreCounter and check
 		if (roundScoreCounter === roundScoreGoal) {
 			winsCounter++;
@@ -37,7 +75,7 @@ $(document).ready(function() {
 
 	*/
 
-	function() {
-
-	}
+	//function() {
+	//
+	//}
 });
